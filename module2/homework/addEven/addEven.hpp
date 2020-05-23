04f6bb1f894369b98f86133dc055fdc6a25ab162
Yoka40 <lucz.artur@gmail.com>
2020-05-23 21:36:22 +0200

    addEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+int addEven(const std::vector<int>& numbers) 
+{
+     int sum = 0; 
+    for(const auto& el: numbers)     
+       {
+         if(!(el%2))
+         {
+           sum += el;
+          }
+       }
+       return sum;
 }
+
